b3fe5627c72d9475c52c080de519d4b5a7318ae6
Todd King <tking@igpp.ucla.edu>
2021-04-22 20:49:51 -0700

    Update version in refcheck.js

--- src/refcheck.js
+++ src/refcheck.js
@@ -23,7 +23,7 @@ const entities = new Entities();
 
 // Configure the app
 var options  = yargs
-	.version('1.0.6')
+	.version('1.0.7')
 	.usage('Perform a check of URL or SPASE ID references in a SPASE resource description.')
 	.usage('$0 [args] <files...>')
 	.example('$0 -i example.xml', 'check SPASE ID references in the given file')
